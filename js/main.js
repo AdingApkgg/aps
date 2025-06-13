b969fb07c57041a58530ef4cdcbc43942cd6760b
AdingApkgg <AdingApkgg@outlook.com>
2025-06-13 13:01:51 +0800

    250613

--- js/main.js
+++ js/main.js
@@ -1,16 +1,23 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const menuToggle = document.querySelector('.menu-toggle');
-    const navMenu = document.querySelector('#nav-menu');
-    menuToggle.addEventListener('click', () => navMenu.classList.toggle('active'));
-    document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-        anchor.addEventListener('click', e => {
-            e.preventDefault();
-            document.querySelector(anchor.getAttribute('href')).scrollIntoView({ behavior: 'smooth' });
-        });
+document.addEventListener("DOMContentLoaded", () => {
+  quicklink.listen({ priority: true });
+  const menuToggle = document.querySelector(".menu-toggle");
+  const navMenu = document.querySelector("#nav-menu");
+  menuToggle.addEventListener("click", () =>
+    navMenu.classList.toggle("active")
+  );
+  document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+    anchor.addEventListener("click", (e) => {
+      e.preventDefault();
+      document
+        .querySelector(anchor.getAttribute("href"))
+        .scrollIntoView({ behavior: "smooth" });
     });
-    const backToTop = document.querySelector('.back-to-top');
-    window.addEventListener('scroll', () => {
-        backToTop.style.display = window.scrollY > 300 ? 'block' : 'none';
-    });
-    backToTop.addEventListener('click', () => window.scrollTo({ top: 0, behavior: 'smooth' }));
+  });
+  const backToTop = document.querySelector(".back-to-top");
+  window.addEventListener("scroll", () => {
+    backToTop.style.display = window.scrollY > 300 ? "block" : "none";
+  });
+  backToTop.addEventListener("click", () =>
+    window.scrollTo({ top: 0, behavior: "smooth" })
+  );
 });
